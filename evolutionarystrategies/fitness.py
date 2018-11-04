5cdbba3ba6c924d4f6f46b659769f5db11f440f4
Frederik Boe Hüttel <s151689@student.dtu.dk>
2018-11-04 14:38:01 +0100

    added TODO's

--- evolutionarystrategies/fitness.py
+++ evolutionarystrategies/fitness.py
@@ -2,8 +2,12 @@
 #import numpy as np
 class fitness():
 
+    # TODO: setup enviroment, add env to init and import pommerman
     def __init__(self):
         self.is_made = True
 
-    def max(self,model, epsilon):
+    def max(self, model, epsilon):
         return sum(model.params)+sum(epsilon)
+
+
+    # TODO: define fitness function for evn
--- evolutionarystrategies/model.py
+++ evolutionarystrategies/model.py
@@ -2,14 +2,19 @@
 import numpy as np
 
 class model():
+    # TODO: change model to be a neural network
     def __init__(self):
         self.params = [0]*10
         self.reward = -1
-
-
+        
+    # TODO: change update params to allow for updateing of neural network.
     def updateparams(self, epsilon, rewards, learningrate):
         best_reward = np.argmax(rewards)
         self.params = self.params + epsilon[best_reward]
 
     def shape(self):
         return len(self.params)
+
+    # TODO: function to mimic agents and to take actions
+    def act(self, state):
+        return 0
