3cec87565585fcc09a5dd08812e9725c6df397e1
ChristianIngwersen <keilstrup@gmail.com>
2018-11-11 18:07:00 +0100

    parallel evolution

--- evolutionarystrategies/evolutionarystrategy.py
+++ evolutionarystrategies/evolutionarystrategy.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import multiprocessing as mp
 
 class EvolutionaryStrategy:
 
@@ -33,3 +34,44 @@ class EvolutionaryStrategy:
         # update the parameters of the model, based on the reward and epsilon
         #print('Avg reward {}'.format(np.mean(rewards)))
         self.model.update_params(epsilons, rewards, self.learning_rate)
+
+    def play_game(self):
+        rewards = []
+        epsilons = []
+        for i in range(self.populationsize):
+            # random noise
+            epsilon = {}
+            for key, shape in self.model.shape().items():
+                if self.model.params[key].type() == "torch.FloatTensor":
+                    epsilon[key] = torch.randn(shape).float()
+                elif self.model.params[key].type() == "torch.LongTensor":
+                    epsilon[key] = torch.randn(shape).long()
+                else:
+                    epsilon[key] = torch.randn(shape)
+
+            # fitness function
+            reward = self.fitness.evaluate(self.model, epsilon, self.learning_rate, 0.5)
+
+        return reward
+
+    def parallel_evolution(self):
+        output = mp.Queue()
+
+        # Setup a list of processes that we want to run
+        permutations = 5
+        processes = [mp.Process(target=self.evolution, args=(x, output)) for x in range(permutations)]
+
+        # Run processes
+        for p in processes:
+            p.start()
+
+        # Exit the completed processes
+        for p in processes:
+            p.join()
+
+        # Get process results from the output queue
+        results = [output.get() for p in processes]
+
+        print(results)
+
+
--- evolutionarystrategies/test.py
+++ evolutionarystrategies/test.py
@@ -9,7 +9,7 @@ evo_strat = EvolutionaryStrategy(model, fitness, learning_rate = 5e-4)
 
 
 for i in range(5):
-    evo_strat.evolution()
+    evo_strat.parallel_evolution()
     if (i-1)%100==0:
     	print("Average win rate over 5 games {}".format(fitness.evaluate(model,0,0,0)))
 
