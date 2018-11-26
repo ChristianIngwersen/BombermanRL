8a26e1798c1313e67f0ef568e4b89b544db6cec4
Christian Ingwersen <keilstrup@gmail.com>
2018-11-26 13:30:22 +0100

    Changed agents

--- evolutionarystrategies/envs/pommerman.py
+++ evolutionarystrategies/envs/pommerman.py
@@ -214,9 +214,9 @@ def make_env(config):
     training_agent = TrainingAgent()
     agent_list = [
         training_agent,
-        pommerman.agents.RandomAgent(),
-        pommerman.agents.RandomAgent(),
-        pommerman.agents.RandomAgent(),
+        pommerman.agents.SimpleAgent(),
+        pommerman.agents.SimpleAgent(),
+        pommerman.agents.SimpleAgent(),
     ]
     if config == "PommeFFAPartialFast-v0":
         env_spec = _ffa_partial_fast_env()
